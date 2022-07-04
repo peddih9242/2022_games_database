b30f1b85e62675422d59886f5fc1ced9e6554f0d
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-07-04 12:01:42 +1200

    config made

---
+++ site_template/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define ("DB_NAME", "peddih9242_00_L2_games");
+define ("DB_USERNAME", "_peddih9242");
+define ("DB_PASSWORD", "nmPdxgy2xYMspdY0");
+define ("DB_HOST", "localhost");
+
+?>
--- site_template/topbit.php
+++ site_template/topbit.php
@@ -51,35 +51,3 @@
             <h1>Games Database</h1>        
         </div> <!-- / banner -->
                        
-                       
-            
-        <div class="box main">
-            <h2>Welcome</h2>
-            
-            
-            <p>
-                Put link to original dataset here
-            </p>
-            
-
-            
-        </div> <!-- / main -->
-        
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC HP 2022
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
