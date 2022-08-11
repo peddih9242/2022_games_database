d4c45c3eb3362c5663b5bf10d0a60026aff27f28
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2022-08-11 15:00:02 +1200

    adding genre to entry form

--- site_template/addentry.php
+++ site_template/addentry.php
@@ -1,5 +1,10 @@
 <?php include("topbit.php");
 
+// get genre list from database
+$genre_sql = "SELECT * FROM `genre` ORDER BY `genre`.`Genre` ASC";
+$genre_query = mysqli_query($dbconnect, $genre_sql);
+$genre_rs = mysqli_fetch_assoc($genre_query);
+
 $app_name = "";
 $subtitle = "";
 $url = "";
@@ -30,31 +35,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 
             <!-- App Name (Required) -->
-            <input class="add-field" type="text" name="app_name" required value="<?php echo $app_name; ?>" placeholder="App Name (required) ..."/>
-
+            <input class="add-field" type="text" name="app_name" value="<?php echo $app_name; ?>" placeholder="App Name (required) ..."/>
 
             <!-- Subtitle (optional) -->
-
+            <input class="add-field" type="text" name="subtitle" value="<?php echo $subtitle; ?>" placeholder="Subtitle (optional) ..."/>
+            
             <!-- URL (required, must start http://) -->
+            <input class="add-field" type="text" name="url" value="<?php echo $url; ?>" placeholder="URL (required) ..."/>
             
             <!-- Genre dropdown (required) -->
+            <select class="adv" name="genre">
+                <option value="" selected>Genre...</option>
 
+                <!-- get options from database -->
+
+            </select>
+            
             <!-- Developer Name (required) -->
-
+            <input class="add-field" type="text" name="dev_name" value="<?php echo $dev_name; ?>" placeholder="Dev Name (required) ..."/>
+            
             <!-- Age (set to 0 if left blank) -->
-
+            <input class="add-field" type="text" name="age" value="<?php echo $age; ?>" placeholder="Age" />
+            
             <!-- Rating (Number between 0 - 5, 1 dp) -->
+            <div>
+                <input class="add-field" type="text" name="rating" value="<?php echo $rating; ?>" placeholder="Rating (0-5)" />
+            </div>
 
             <!-- # of ratings (integer more than 0) -->
-
+            <input class="add-field" type="text" name="rate_count" value="<?php echo $rate_count; ?>" placeholder="# of Ratings" />
+            
             <!-- Cost (Decimal 2dp, must be more than 0) -->
-
+            <input class="add-field" type="text" name="cost" value="<?php echo $cost; ?>" placeholder="Cost" />
+            
             <!-- In App Purchase radio buttons -->
 
             <!-- Description text area -->
-
-            <!-- Submit Button -->
+            <input class="add-field" type="text" name="description" value="<?php echo $description; ?>" placeholder="Description (required) ..."/>
             
+            <!-- Submit Button -->
             <p>
                 <input class="submit advanced-button" type="submit" value="Submit" />
             </p>
